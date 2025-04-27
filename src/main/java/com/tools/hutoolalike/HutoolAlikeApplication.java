262b836d8d4e2d652a621fdaadf7eb20117bbcb9
nacycher <nacycher@163.com>
2025-04-27 10:59:00 +0800

    project init

---
+++ src/main/java/com/tools/hutoolalike/HutoolAlikeApplication.java
@@ -0,0 +1,13 @@
+package com.tools.hutoolalike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HutoolAlikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HutoolAlikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tools/hutoolalike/HutoolAlikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tools.hutoolalike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HutoolAlikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
